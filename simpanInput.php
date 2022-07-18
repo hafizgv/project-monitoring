450a287e36c162344315ea5f8bcd65584ad521a5
hafizgv <94153431+hafizgv@users.noreply.github.com>
2022-07-18 15:25:43 +0700

    Update simpanInput.php

--- simpanInput.php
+++ simpanInput.php
@@ -5,11 +5,12 @@ if(isset($_POST['submit'])){
     $project = $_POST['project'];
     $client = $_POST['client'];
     $foto = $_POST['foto'];
+    $email = $_POST['email'];
     $nama = $_POST['nama'];
     $mulai = $_POST['tgl_mulai'];
     $akhir = $_POST['tgl_akhir'];
     
-    $query = mysqli_query($conn, "INSERT INTO project_manage(project_name, client, foto, name, start_at, finish_at) VALUES('$project', '$client', '$foto', '$nama', '$mulai', '$akhir')");
+    $query = mysqli_query($conn, "INSERT INTO project_manage(project_name, client, foto, email, name, start_at, finish_at) VALUES('$project', '$client', '$foto', '$email', '$nama', '$mulai', '$akhir')");
     
     if($query){
         header ("location:dashboard.php");
